87d68f62adf6e7917f073393e232115851cc0088
Roman Coedo <romancoedo@gmail.com>
2017-05-12 10:15:32 +0200

    Remove the autoinstall feature

--- conf.d/anyenv.fish
+++ conf.d/anyenv.fish
@@ -3,11 +3,14 @@ if test -e $HOME/.anyenv
   __add_to_path $ANYENV_ROOT/bin
   status --is-interactive; and source (anyenv init -|psub)
 else
-  if type -q git
-    if __confirm "~/.anyenv dir not found, do you want to install it?"
-      git clone https://github.com/riywo/anyenv ~/.anyenv
-    end
-  else
-    echo ~/.anyenv dir not found and git is not installed. You are on your own!
-  end
+    echo ~/.anyenv dir not found.\nYou can run 'git clone https://github.com/riywo/anyenv ~/.anyenv' to install it.
+
+    # https://github.com/fish-shell/fish-shell/issues/3805
+    #  if type -q git
+    #    if __confirm "~/.anyenv dir not found, do you want to install it?"
+    #      git clone https://github.com/riywo/anyenv ~/.anyenv
+    #    end
+    #  else
+    #    echo ~/.anyenv dir not found and git is not installed. You are on your own!
+    #  end
 end
